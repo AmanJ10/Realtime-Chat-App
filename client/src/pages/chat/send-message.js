aa5a7e99d0751440dcef94cd83c111defc424a04
AmanJ10 <joharapurkaraman95@gmail.com>
2023-12-04 08:54:09 +0530

    Update send-message.js

--- client/src/pages/chat/send-message.js
+++ client/src/pages/chat/send-message.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import Picker from '@emoji-mart/react';
-import data from '@emoji-mart/data'
 import styles from './styles.module.css';
 
 const SendMessage = ({ socket, username, room }) => {
@@ -15,31 +13,7 @@ const SendMessage = ({ socket, username, room }) => {
     }
   };
 
-  const handleEmojiSelect = (emoji) => {
-    setMessage((prevMessage) => prevMessage + (emoji.id || emoji.colons));
-  };
 
-  return (
-    <div className={styles.sendMessageContainer}>
-      <div style={{ position: 'relative' }}>
-        <input
-          className={styles.messageInput}
-          placeholder='Message...'
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <button className={`${styles.btn} ${styles.btnPrimary}`} onClick={sendMessage}>
-          Send Message
-        </button>
-        <button className={`${styles.btn} ${styles.btnPrimary}`} onClick={() => setShowEmojiPicker(!showEmojiPicker)}>
-          😊
-        </button>
-        {showEmojiPicker && (
-          <Picker data={data} onEmojiSelect={handleEmojiSelect} />
-        )}
-      </div>
-    </div>
-  );
 };
 
 export default SendMessage;
